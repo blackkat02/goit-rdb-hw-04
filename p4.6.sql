b26f38e0282db45174635d8554480cc1594eb303
Borys <boris02374@gmail.com>
2025-07-06 20:00:22 +0300

    added task 4.6-4.7

---
+++ p4.6.sql
@@ -0,0 +1,19 @@
+USE hw_3db;
+
+SELECT
+    cat.id AS CategoryID,   
+    cat.name AS CategoryName,  
+    COUNT(*) AS TotalItemsInOrders,  
+    AVG(od.quantity) AS AverageQuantity 
+FROM
+    order_details od  
+INNER JOIN
+    products p ON od.product_id = p.id 
+INNER JOIN
+    categories cat ON p.category_id = cat.id 
+GROUP BY
+    cat.id,
+    cat.name 
+HAVING 
+	AVG(od.quantity) > 21
+ORDER BY COUNT(*) DESC;
